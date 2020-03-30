7656e10aeec4b462e51b426fb32fe232ff35fa86
Victor Plushko <vicplushko33@gmail.com>
2020-03-30 06:41:34 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,8 @@ def oxford_comma(array)
   new_array = []
   array.each_with_index do |item, index|
     if (index == 0)
-      new_array << "#{item}'
+      new_array << "#{item}"
     elsif (index == array.size -1)
-      new array << " and "
+      new_array << " and #{item} "
     
 end
