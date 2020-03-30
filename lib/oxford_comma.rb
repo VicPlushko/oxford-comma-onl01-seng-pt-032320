c06ca0e90df374bea8e839d9278c1f314fddc7dc
Victor Plushko <vicplushko33@gmail.com>
2020-03-30 07:19:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     return array[0]
   elsif (array.size == 2)
     return "#{array[0] and #{array[1]}"
-  end
+    
   new_array = []
   array.each_with_index do |item, index|
     if (array.size == 1)
