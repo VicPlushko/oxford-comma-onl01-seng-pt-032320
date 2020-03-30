debc606fed3e78dacc6da6d0c70b62819f5e4e8d
Victor Plushko <vicplushko33@gmail.com>
2020-03-30 05:37:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,3 @@
 def oxford_comma(array)
-  if array == 1
-   return array.join(", ")
-  elsif array == 2
-   return array.join("and ")
-  else 
-    return array.join()
-   
-   
+   array.join(", ")
 end
